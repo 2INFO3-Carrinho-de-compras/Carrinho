0c3f914aa2908637351ed8c7e1814aa0f53cb6c6
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2025-04-26 14:41:03 -0300

    FEAT: Término do rodapé #3

--- carrinho-de-compras/src/App.vue
+++ carrinho-de-compras/src/App.vue
@@ -1,108 +1,123 @@
 <script setup> 
 </script> 
 
-<template>   
-<header>     
-    <h1>
-        IFbooks
-    </h1>     
-    <p class="titulo">       
-        Apreço a <br> leitura     
-    </p>       
-    <input type="text" class="pesquisa" placeholder="Pesquisar" >     
-    <div class="topo">       
-        <ul>           
-            <li>             
-                <a href="Termos">Termos</a>           
-            </li>            
-            <li>             
-                <a href="Equipe">Equipe</a>           
-            </li>            
-            <li>             
-                <a href="Envio">Envio</a>           
-            </li>            
-            <li>             
-                <a href="Devoluções">Devoluções</a>           
-            </li>            
-            <li>             
-                <i class="fa-solid fa-square-plus"></i>           
-            </li>            
-            <li>             
-                <i class="fa-solid fa-heart"></i>           
-            </li>            
-            <li>             
-                <i class="fa-solid fa-user"></i>           
-            </li>       
-        </ul>     
-    </div> 
-</header>
+<template>
+    <footer>
+        <div class="superior">
+            <div class="esquerda">
+                <h2>
+                    IFbooks
+                </h2>
+                <ul>
+                    <li>
+                        <i class="fa-brands fa-square-facebook"></i>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-instagram"></i>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-square-twitter"></i>
+                    </li>
+                </ul>
+            </div>
 
-<main>   
-    <section>     
-        <div class="Conteudo">       
-            <button>         
-                Autor de Abril       
-            </button>       
-            <h2>         
-                Eric-Emanuel Schmitt       
-            </h2>       
-            <p>         
-                Eric-Emmanuel Schmitt has been awarded more than <br> 20 literary prizes and distinctions, and in 2001 he received the <br> title of Chevalier des Arts et des Lettres. His books have been translated into over <br> 40 languages.       
-            </p>       
-            <button>         
-                Acessar página do livro       
-            </button>     
-        </div>   
-    </section>  
-</main> 
+            <div class="direita">
+                <h2>
+                    Contato
+                </h2>
+                <ul class="contato">
+                    <li>
+                        <i class="fa-solid fa-hashtag"></i> 
+                        <p>+55 47 40045263</p>               
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-clock"></i>
+                        <p>8h às 23h - Seg a Sex</p>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-envelope"></i>
+                        <p>contato@ifbooks.com</p>
+                    </li>
+                </ul>
+
+                <ul class="pagamento">
+                    <li>
+                        <img src="/public/img/paipal.png" alt="paypal">
+                    </li>
+                    <li>
+                        <img src="/public/img/MasterCard-Logo.png" alt="mastercard">
+                    </li>
+                    <li>
+                        <img src="/public/img/VISA-card-logo.png" alt="visa">
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="inferior">
+            <p>© Alguns direitos reservados. IFbooks 2025. </p>
+        </div>
+    </footer>
 </template>
 
 <style scoped> 
-header {   
-    display: flex;   
-    border-bottom:2px solid rgb(76, 196, 76); 
-} 
-h1{   
-    padding: 0 10px 0 0;   
-    font-size: 1vw;   
-    margin: 0.9vw 0 0.9vw 8vw; 
-} 
-header input{   
-    border-radius: 5px;   
-    border: none;   
-    background-color: rgb(235, 235, 235);   
-    padding: 0 10vw 0 1vw;   
-    margin: 0.9vw 0 0.9vw 0; 
-} 
-div.topo ul{   
-    display: flex;   
-    padding: 1vw 0 0 0; 
-} 
-div.topo ul li {   
-    margin: 0 3vw 0 0;   
-    list-style: none; 
-} 
-header p {   
-    display: flex;   
-    margin: 1vw 0 0 0; 
-} 
-a {   
-    color: black;   
-    text-decoration: none; 
-} 
-header p.titulo{   
-    padding: 0 0 0 10px;   
-    font-size: 0.7vw;   
-    margin: 0.9vw 8vw 0.9vw 0;   
-    color: rgb(76, 196, 76);   
-    border-left:2px solid rgb(76, 196, 76); 
-} 
-section {   
-    display: flex;   
-    justify-content: space-around; 
-} 
-section div.foto {   
-    display: flex;   
-    justify-content: left;
-} 
+footer{
+    background-color: rgb(211, 34, 137);
+    color: white;
+}
+.superior{
+    display: flex;
+    justify-content: space-between;
+}
+.inferior{
+    text-align: center;
+    margin: 5vw 0 0 0;
+    padding: 1.5vw 0 1.5vw 0;
+    border-top: 1px solid white;
+    color: rgb(255, 145, 209);
+}
+.esquerda{
+    margin: 2vw 0 0 6vw;
+}
+.esquerda h2{
+    font-size: 1.3vw;
+    margin: 0 0 0.5vw 2.7vw;
+}
+.esquerda ul{
+    display: flex;
+    list-style: none;
+}
+.esquerda ul li{
+    font-size: 2vw;
+    margin: 0 2vw 0 0;
+}
+.direita{
+    margin: 2vw 0 0 10vw;
+}
+.direita h2{
+    font-size: 1.3vw;
+    margin: 0 0 0.5vw 2.7vw;
+    font-weight: bold;
+}
+.direita ul.contato{
+    list-style: none;
+}
+.direita ul.contato li{
+    display: flex;
+    margin: 0 2vw 0 0;
+    padding: 0 2vw 0 0;
+}
+.direita ul.contato li i{
+    padding: 1.2vw 1vw 0 0;
+}
+.direita ul.contato li p{
+    padding: 0.7vw 2vw 0 0;
+}
+.direita ul.pagamento{
+    list-style: none;
+    display: flex;
+}
+.direita ul.pagamento li{
+    margin: 7vw 0 0 0;
+    padding: 0 1vw 0 0;
+}
 </style>
